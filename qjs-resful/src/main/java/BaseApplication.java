fe63576daa36a7e2a98cab68fbb1def28a20339b
krui <kruiwangyi@163.com>
2017-05-24 22:30:42 +0800

    springboot+mybatiesplus项目环境搭建初始化

---
+++ qjs-resful/src/main/java/BaseApplication.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by krui on 2017/05/23.
+ */
+@SpringBootApplication
+public class BaseApplication {
+    public static void main( String[] args )
+    {
+        SpringApplication.run(BaseApplication.class, args);
+    }
+
+}
